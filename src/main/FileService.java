4a2e32c6003b59e6589b2b23504b6aa94008cd77
rmit-s3511667-Ali-Alahmari <32062234+AliAla@users.noreply.github.com>
2018-04-25 17:43:19 +1000

    FileService Class

---
+++ src/main/FileService.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package main;
+
+import java.io.*;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ * @author alial
+ *
+ */
+public class FileService {
+
+	String fileName = "jMoSS.txt";
+
+	/*
+	 * Create method to write on file
+	 */
+	
+	// Parameters we can add them as we need 
+	public void write(String theaterLocation, String movieName, String date, String time) {
+
+		try {
+			FileWriter fw = new FileWriter(fileName, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter pw = new PrintWriter(bw);
+
+			pw.println(theaterLocation + "," + movieName + "," + date + "," + time);
+			pw.flush();
+			pw.close();
+
+			JOptionPane.showMessageDialog(null, "Information saved");
+			
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Information not saved");
+
+		}
+	}
+
+	/*
+	 * Create method to read from file
+	 */
+
+	public void read() {
+		File file = new File(fileName);
+		try {
+
+			Scanner inputStream = new Scanner(file);
+			inputStream.nextLine(); // ignore the first line
+			while (inputStream.hasNext()) {
+				String data = inputStream.nextLine();// gets a whole line
+
+				System.out.println(data);
+
+			}
+			inputStream.close();
+		} catch (IOException e) {
+			System.out.println("File Not Found!!");
+		}
+	}
+}
+
