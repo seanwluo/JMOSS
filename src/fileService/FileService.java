736b1db3ec0f822f10eb92619aec8c571c9d10e5
rmit-s3511667-Ali-Alahmari <32062234+AliAla@users.noreply.github.com>
2018-04-26 20:53:36 +1000

    write method is generic method now

--- src/fileService/FileService.java
+++ src/fileService/FileService.java
@@ -13,21 +13,21 @@ import javax.swing.JOptionPane;
  */
 public class FileService {
 
-	private String fileName = "jMoSS.txt";
+	private static String fileName = "jMoSS.txt";
 
 	/*
 	 * Create method to write on file
 	 */
 	
 	// Parameters we can add them as we need 
-	public void write(String theaterLocation, String movieName, String date, String time) {
+	public static <T>void write(T object) {
 
 		try {
 			FileWriter fw = new FileWriter(fileName, true);
 			BufferedWriter bw = new BufferedWriter(fw);
 			PrintWriter pw = new PrintWriter(bw);
 
-			pw.println(theaterLocation + "," + movieName + "," + date + "," + time);
+			pw.println(object);
 			pw.flush();
 			pw.close();
 
