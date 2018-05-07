8645c68cf27ea1e5417648c7d0ef3933d4a821f0
Raj Kumar Shrestha <s3675667@student.rmit.edu.au>
2018-05-07 18:28:16 +1000

    Add Book model and service

---
+++ src/Service/BookService.java
@@ -0,0 +1,81 @@
+package Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fileService.FileService;
+import model.Book;
+import model.Seat;
+
+public class BookService
+{
+	private final FileService fService = new FileService("seat.txt");
+	private List<String> bookingData;
+	private Seat seat;
+	private SeatService seatService;
+	
+	public BookService(SeatService seatService, Seat seat)
+	{
+		this.seat = seat;
+		this.seatService = seatService;
+		this.bookingData = fService.read();
+	}
+	
+	public Book findById(String seatId)
+	{
+		Book book = null;
+		
+		for(String booking: bookingData) {
+			String[] data = booking.split(",");
+			
+			String sessionSeatId = data[0].trim();
+			if(sessionSeatId.equals(seatId))
+			{
+				String email = data[1].trim();
+				String suburb = data[2].trim();
+				String time = data[3].trim();
+				
+				book = new Book(seat);
+				book.setEmail(email);
+				book.setSuburb(suburb);
+				book.setTime(time);
+				
+				break;
+			}
+		}
+		
+		return book;
+	}
+	
+	public void createBooking(String email, String suburb) {
+		Book book = new Book(seat);
+		
+		book.setEmail(email);
+		book.setEmail(suburb);
+	}
+
+	public void removeBooking(String seatSessionId) {
+		List<String> newBookingData = new ArrayList<String>();
+		boolean deleted = false;
+		for(String booking: bookingData)
+		{
+			String[] data = booking.split(",");
+			String seatId = data[0].trim();
+			if(seatId.equals(seatId))
+			{	
+				deleted = true;
+				seatService.removeBooking(seat);
+			} else {
+				newBookingData.add(booking);
+			}
+		}
+		
+		bookingData = newBookingData;
+		
+		if(deleted) {
+			System.out.print("\nDeleted successfully.");
+		} else {
+			System.out.print("\nRecord noy found.");
+		}
+	}
+}
---
+++ src/model/Book.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Book {
+	Seat seat;
+	String seatSesionId;
+	String email;
+	String suburb;
+	String time;
+	
+	public Book(Seat seat)
+	{
+		this.seat = seat;
+	}
+
+	public Seat getSeat() {
+		return seat;
+	}
+
+	public String getSeatSesionId() {
+		return seatSesionId;
+	}
+
+	public void setSeatSesionId(String seatSesionId) {
+		this.seatSesionId = seatSesionId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSuburb() {
+		return suburb;
+	}
+
+	public void setSuburb(String suburb) {
+		this.suburb = suburb;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+}
